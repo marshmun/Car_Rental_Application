e990748ff20cb4ac677707e06fe7cb1ad4e32f5c
Marshall Munoz <Marshall.Munoz@fbitn.com>
2017-09-29 10:49:59 -0500

    consolidating function of pages, reducing HTML pages

---
+++ src/com/rental/servlet/DeleteCarServlet.java
@@ -0,0 +1,63 @@
+package com.rental.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class DeleteCarServlet
+ */
+@WebServlet("/DeleteCarServlet")
+public class DeleteCarServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteCarServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		//get information of the car to be deleted and admins password
+		String carid = req.getParameter("ID");
+		String password = req.getParameter("Password");
+		
+		ResultSet rs =null;
+		Connection conn = null;
+		Statement st= null;
+
+		try {
+		Context    ctx = new InitialContext();
+	    Context env = ( Context )ctx.lookup( "java:comp/env" );
+	    DataSource ds = ( DataSource )env.lookup( "jdbc/carRentalSystem");
+		conn = ds.getConnection();
+		st= conn.createStatement();
+		
+		
+	}
+
+}
