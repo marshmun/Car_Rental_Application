c74c2d201c5212f9493d3c23f61265b60042bbf6
Marshall Munoz <Marshall.Munoz@fbitn.com>
2017-10-03 09:16:35 -0500

    admin can now make user an admin, using username changes to this will be made acordingly

--- src/com/rental/servlet/AdminAddAdminServlet.java
+++ src/com/rental/servlet/AdminAddAdminServlet.java
@@ -1,11 +1,16 @@
 package com.rental.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class AdminAddAdminServlet
@@ -33,9 +38,39 @@ public class AdminAddAdminServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String username = req.getParameter("User_Name");
+		
+		
+		int rs;
+		Connection conn = null;
+		java.sql.PreparedStatement st= null;
+		String nativeSQL = "";
+		
 
+		try {
+			Context    ctx = new InitialContext();
+		    Context env = ( Context )ctx.lookup( "java:comp/env" );
+		    DataSource ds = ( DataSource )env.lookup( "jdbc/carRentalSystem");
+			conn = ds.getConnection();
+
+			st = conn.prepareStatement("update userdetails SET User_Type = 'Admin' where User_Name='"+ username+ "'");
+			st.clearParameters();
+			rs= st.executeUpdate();
+					if(rs != 0) {
+						res.sendRedirect("addAdmin.html");
+						return;
+					}else {
+						
+					}
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+				}
+			finally {
+	            try{ if(st != null ) st.close(); } catch(java.sql.SQLException e){}
+	            try{ if(conn != null ) conn.close(); } catch(java.sql.SQLException e){}
+
+			}
+		}
 }
